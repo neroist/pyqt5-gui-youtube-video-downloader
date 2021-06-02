fcd886bc96893cbad86e072680c3a8c91e85d71e
t-h-rk-turner <71654487+t-h-rk-turner@users.noreply.github.com>
2021-06-01 21:39:41 -0400

    Update download-popup.py

--- download-popup.py
+++ download-popup.py
@@ -18,7 +18,7 @@ class DownloadPopup(QDialog):
 
         self.layout = QVBoxLayout()
 
-        path = bytearray(QUrl.fromLocalFile("file:///"+video_path).toEncoded()).decode() # i have no idea what this means or does
+        path = bytearray(QUrl.fromLocalFile("file:///"+video_path).toEncoded()).decode() # i have no idea what this means or does, but it works!
         message = QLabel(f'The video was just downloaded. Click <a href="{path}">here</a> to see the video in it\'s folder.', self)
         message.setOpenExternalLinks(True)
         self.layout.addWidget(message)
